13d80e787acdb5d89cfaaa8c0c869f141be05188
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-03-18 13:00:18 +0100

    Aggiungere i file di progetto.

---
+++ Matrice4/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Matrice4
+{
+    internal class Program
+    {
+        static void Stampa(int[,] m, int[,] m1)
+        {
+            for(int i=0; i<m.GetLength(0); i++)
+            {
+                for(int j=0; j < m.GetLength(1); j++)
+                {
+                    Console.Write($"[{m[i, j]}]");
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write($"[{m1[i, j]}]");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static int[,] Trasporta(int[,] m, int[,] m1)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    m1[j, i] = m[i, j];
+                }
+                
+            }
+
+            return m1;
+        }
+        static void Main(string[] args)
+        {
+            int[,] m = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] m1 = new int[m.GetLength(0), m.GetLength(1)];
+
+            Stampa(m, m1);
+
+            Console.WriteLine();
+
+            m1 = Trasporta(m, m1);
+
+            Stampa(m, m1);
+        }
+    }
+}
---
+++ Matrici5/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Matrice4
+{
+    internal class Program
+    {
+        static void Stampa(int[,] m)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    Console.Write($"[{m[i, j]}]");
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+
+        static int[,] Trasporta(int[,] m, int[,] m1)
+        {
+            for (int i = 0; i < m.GetLength(0); i++)
+            {
+                for (int j = 0; j < m.GetLength(1); j++)
+                {
+                    if (m.GetLength(0) == m1.GetLength(1))
+                    {
+                        m1[j, i] = m[i, j];
+                    }
+                }
+
+            }
+
+            return m1;
+        }
+        static void Main(string[] args)
+        {
+            int[,] m = { { 1, 2 }, { 3, 4 }, { 6, 7 } };
+            int[,] m1 = new int[m.GetLength(1), m.GetLength(0)];
+
+            Stampa(m);
+
+            Console.WriteLine();
+
+            Stampa(Trasporta(m, m1));
+        }
+    }
+}
+
